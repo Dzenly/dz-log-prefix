f9ff1d2accedeefc2112221fa732df991861ea00
Dzenly <alexchem@yandex.ru>
2017-11-15 13:25:46 +0700

    Corrected example and readme.

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 Adds prefixes to winston - like JS loggers.
 
+For usage examples, see: https://github.com/Dzenly/dz-log-prefix/blob/master/test/test.js.
+
+
--- test/test.js
+++ test/test.js
@@ -3,13 +3,15 @@
 const winston = require('winston');
 const dzLogPrefix = require('../index.js');
 
-
 const prefixedLogger = dzLogPrefix.addPrefixToCommonLogFuncs(winston, 'My Prefix: ');
 
+// info: Winston info log.
 winston.info('Winston info log.');
 
+// info: My Prefix: prefixedLogger info log.
 prefixedLogger.info('prefixedLogger info log.');
 
 const doublePrefixedLogger = dzLogPrefix.addPrefixToCommonLogFuncs(prefixedLogger, 'My PostPrefix: ');
 
+// info: My Prefix: My PostPrefix: doublePrefixedLogger info log.
 doublePrefixedLogger.info('doublePrefixedLogger info log.');

f58a8edd4803c75ec831b9bb98da47f4198f8161
Dzenly <alexchem@yandex.ru>
2017-11-18 10:18:02 +0700

    Example extended.

--- test/test.js
+++ test/test.js
@@ -3,15 +3,20 @@
 const winston = require('winston');
 const dzLogPrefix = require('../index.js');
 
-const prefixedLogger = dzLogPrefix.addPrefixToCommonLogFuncs(winston, 'My Prefix: ');
+const prefixedLogger1 = dzLogPrefix.addPrefixToCommonLogFuncs(winston, 'My Prefix1: ');
+
+const prefixedLogger2 = dzLogPrefix.addPrefixToCommonLogFuncs(winston, 'My Prefix2: ');
 
 // info: Winston info log.
 winston.info('Winston info log.');
 
-// info: My Prefix: prefixedLogger info log.
-prefixedLogger.info('prefixedLogger info log.');
+// info: My Prefix1: prefixedLogger1 info log.
+prefixedLogger1.info('prefixedLogger1 info log.');
 
-const doublePrefixedLogger = dzLogPrefix.addPrefixToCommonLogFuncs(prefixedLogger, 'My PostPrefix: ');
+// info: My Prefix2: prefixedLogger2 info log.
+prefixedLogger2.info('prefixedLogger2 info log.');
 
-// info: My Prefix: My PostPrefix: doublePrefixedLogger info log.
-doublePrefixedLogger.info('doublePrefixedLogger info log.');
+const doublePrefixedLogger1 = dzLogPrefix.addPrefixToCommonLogFuncs(prefixedLogger1, 'My PostPrefix1: ');
+
+// info: My Prefix1: My PostPrefix1: doublePrefixedLogger1 info log.
+doublePrefixedLogger1.info('doublePrefixedLogger1 info log.');
